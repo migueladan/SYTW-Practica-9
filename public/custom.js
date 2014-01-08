29f40c90424ad7ad661c89afa9e622041bf56845
migueladan <alu0100356137@ull.edu.es>
2014-01-08 02:30:12 +0000

    Ventana emergente al clickear sobre un nick

--- public/custom.js
+++ public/custom.js
@@ -14,11 +14,13 @@ es.onmessage = function(e){
 //$("#chat-submit").live("submit", function(e){
 $("#chat-submit").submit(function(e){
     var messages_box = $("#message");
-    $.post('/chat', {
-        message: $('#message').val()
-    });
-    messages_box.val('');
-    messages_box.focus();
+    
+        $.post('/chat', {
+            message: $('#message').val()
+        });
+        messages_box.val('');
+        messages_box.focus();
+
     e.preventDefault();
 });
 
@@ -28,12 +30,29 @@ user_source.onmessage = function(e){
     var users_div = $("#users");
     
     users_div.empty();
-    var list = $("<ul>");
+    var list = $("<ul id='tipo-lista'>");
+    var privis = $("#privados");
+
     users_div.append(list);
     
     names = JSON.parse(e.data);
     
     for (var i = 0; i < names.num; i++) {
-        list.append($("<li>").text(names.users[i]));
+        list.append( $("<li class='usuario-lista' id='u-" + names.users[i] + "'  >").text(names.users[i]) );
     }
+    list.on("click", "li", function(){                
+            //alert("click en:"+$(this).attr("id") );
+            //var interior = '<div class="privado-interior"> <div class="privado-mensajes"></div>   </div>';
+            var ejem = $('<div class="privado" title='+$(this).attr("id").substring(2) +' id =v-'+ $(this).attr("id").substring(2) +' >').append("<p>Esto es un privado<p>");
+
+            privis.append( ejem );
+            ejem.dialog();
+
+    });
+    
+    /*privis.on("click", ".privado", function() {
+        $(this).dialog();
+    });*/
+
 };
+    
